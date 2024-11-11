1b7d376b74cd507c855b65ea68b9a71e8c09a4d7
Markus Himmel <markus@lean-fro.org>
2024-11-11 13:43:34 +0100

    Remove value

--- Main.lean
+++ Main.lean
@@ -2,10 +2,10 @@ set_option autoImplicit false
 
 universe u v
 
-variable {α : Type} {β : Type}
+variable {α : Type}
 
-inductive TreeNode (α : Type) (β : Type) where
-  | inner (size : Nat) (k : α) (v : β) (l r : TreeNode α β)
+inductive TreeNode (α : Type) where
+  | inner (size : Nat) (k : α) (l r : TreeNode α)
   | leaf
 
 namespace TreeNode
@@ -16,88 +16,63 @@ def delta : Nat := 3
 def ratio : Nat := 2
 
 @[inline]
-def size : TreeNode α β → Nat
-  | inner s _ _ _ _ => s
+def size : TreeNode α → Nat
+  | inner s _ _ _ => s
   | leaf => 0
 
-instance : Inhabited (TreeNode α β) where
+instance : Inhabited (TreeNode α) where
   default := .leaf
 
-@[inline] def balanceL (k : α) (v : β) (l r : TreeNode α β) : TreeNode α β :=
-  match r with
-  | leaf => match l with
-    | leaf => .inner 1 k v .leaf .leaf
-    | l@(inner _ _ _ .leaf .leaf) => .inner 2 k v l .leaf
-    | inner _ lk lv .leaf (.inner _ lrk lrv _ _) =>
-        .inner 3 lrk lrv (.inner 1 lk lv .leaf .leaf) (.inner 1 k v .leaf .leaf)
-    | inner _ lk lv ll@(.inner _ _ _ _ _) .leaf =>
-        .inner 3 lk lv ll (.inner 1 k v .leaf .leaf)
-    | _ => panic! "Unexpected"
-  | r@(inner rs _ _ _ _) => match l with
-    | leaf => .inner (1 + rs) k v .leaf r
-    | l@(inner ls lk lv ll lr) =>
-        if ls > delta * rs then match ll, lr with
-          | inner lls _ _ _ _, inner lrs lrk lrv lrl lrr =>
-              if lrs < ratio * lls then .inner (1 + ls + rs) lk lv ll (.inner (1 + rs + lrs) k v lr r)
-              else .inner (1 + ls + rs) lrk lrv (.inner (1 + lls + lrl.size) lk lv ll lrl) (.inner (1 + rs + lrr.size) k v lrr r)
-          | _, _ => panic! "Unexpected case"
-        else .inner (1 + ls + rs) k v l r
-
-@[inline] def balanceR (k : α) (v : β) (l r : TreeNode α β) : TreeNode α β :=
+@[inline] def balanceR (k : α) (l r : TreeNode α) : TreeNode α :=
   match l with
   | leaf => match r with
-    | leaf => .inner 1 k v .leaf .leaf
-    | r@(inner _ _ _ .leaf .leaf) => .inner 2 k v .leaf r
-    | inner _ rk rv .leaf rr@(.inner _ _ _ _ _) => .inner 3 rk rv (.inner 1 k v .leaf .leaf) rr
-    | inner _ rk rv (.inner _ rlk rlv _ _) .leaf => .inner 3 rlk rlv (.inner 1 k v .leaf .leaf) (.inner 1 rk rv .leaf .leaf)
+    | leaf => .inner 1 k .leaf .leaf
+    | r@(inner _ _ .leaf .leaf) => .inner 2 k .leaf r
+    | inner _ rk .leaf rr@(.inner _ _ _ _) => .inner 3 rk (.inner 1 k .leaf .leaf) rr
+    | inner _ rk (.inner _ rlk _ _) .leaf => .inner 3 rlk (.inner 1 k .leaf .leaf) (.inner 1 rk .leaf .leaf)
     | _ => panic! "Unexpected"
-  | l@(inner ls _ _ _ _) => match r with
-    | leaf => .inner (1 + ls) k v l .leaf
-    | r@(inner rs rk rv rl rr) =>
+  | l@(inner ls _ _ _) => match r with
+    | leaf => .inner (1 + ls) k l .leaf
+    | r@(inner rs rk rl rr) =>
         if rs > delta * ls then match rl, rr with
-          | inner rls rlk rlv rll rlr, .inner rrs _ _ _ _ =>
-              if rls < ratio * rrs then .inner (1 + ls + rs) rk rv (.inner (1 + ls + rls) k v l rl) rr
-              else .inner (1 + ls + rs) rlk rlv (.inner (1 + ls + rll.size) k v l rll) (.inner (1 + rrs + rlr.size) rk rv rlr rr)
+          | inner rls rlk rll rlr, .inner rrs _ _ _ =>
+              if rls < ratio * rrs then .inner (1 + ls + rs) rk (.inner (1 + ls + rls) k l rl) rr
+              else .inner (1 + ls + rs) rlk (.inner (1 + ls + rll.size) k l rll) (.inner (1 + rrs + rlr.size) rk rlr rr)
           | _, _ => panic! "Unexpected case"
-        else .inner (1 + ls + rs) k v l r
+        else .inner (1 + ls + rs) k l r
 
-@[specialize] def insert (cmp : α → α → Ordering) (k : α) (v : β) : TreeNode α β → TreeNode α β
-| leaf => .inner 1 k v .leaf .leaf
-| inner sz ky y l r => match cmp k ky with
-    | .lt => balanceL ky y (insert cmp k v l) r
-    | .eq => .inner sz ky v l r
-    | .gt => balanceR ky y l (insert cmp k v r)
+def insert (k : α) : TreeNode α → TreeNode α
+| leaf => .inner 1 k .leaf .leaf
+| inner _ ky l r => balanceR ky l (insert k r)
 
 end TreeNode
 
 open Lean
 
-def mkStrings (num : Nat) : Array Nat := Id.run do
-  let mut ans := #[]
-  for i in [0:num] do
-    ans := ans.push i
-  for i in [0:num/2] do
-    ans := ans.push (2 * i)
-  ans
-
-@[noinline] def mkMyStrings (num : Nat) : IO (Array Nat) := do
-  return  (mkStrings num)
-
 def sz : Nat := 300000
 
-@[noinline] def growInsertB (strings : Array Nat) : TreeNode Nat Nat := Id.run do
-  let mut m : TreeNode Nat Nat := .leaf
+@[noinline] def growInsertB (numbers : Array Nat) : IO (TreeNode Nat) := do
+  let mut m : TreeNode Nat := .leaf
   let mut i := 0
-  for s in strings do
-    m := m.insert Ord.compare s i
+  for s in numbers do
+    m := m.insert s
     i := i + 1
   return m
 
-@[noinline] def growInsertB' (a : Array Nat) : IO (TreeNode Nat Nat) := do
-  return growInsertB a
+-- def p : Nat := 10^9 + 7
+
+-- def powModNaive (a b p : Nat) : IO Nat := do
+--   let mut ans : Nat := 1
+--   let mut q := 0
+--   for _ in [:b] do
+--     ans := (ans * a) % p
+--     if ans > 100 then
+--       q := q + 1
+--   return q
 
 def main : IO Unit := do
-  let strings ← mkMyStrings sz
+  let numbers := Array.range sz
   for _ in [:5] do
-    let _ ← timeit "" $ growInsertB' strings
+    -- let _ ← timeit "" $ powModNaive 2 10000000 p
+    let _ ← timeit "" $ growInsertB numbers
   println! "-------------------"
